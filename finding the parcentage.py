2ec36525bc90b0befcab8732ad430bc57d5f3c59
Nasim uddin <103178419+nasimabo@users.noreply.github.com>
2023-12-25 09:41:37 +0600

    finding the parcentage

---
+++ finding the parcentage.py
@@ -0,0 +1,48 @@
+"""The provided code stub will read in a dictionary containing key/value
+pairs of name:[marks] for a list of students. Print the average of the marks
+array for the student name provided, showing 2 places after the decimal."""
+
+
+"""def average_marks(student_marks, query_name):
+    if query_name in student_marks:
+        marks = student_marks[query_name]
+        average_score = sum(marks) / len(marks)
+        return "{:.2f}".format(average_score)
+    else:
+        return "Student not found in records."
+
+# Sample Input
+num_students = int(input())
+student_marks = {}
+
+for _ in range(num_students):
+    name, *marks = input().split()
+    student_marks[name] = list(map(float, marks))
+
+query_name = input()
+
+# Calculate and print the average for the query_name
+print(average_marks(student_marks, query_name))"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
